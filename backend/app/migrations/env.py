c6066bf950e8ac892ea3edf719fdd27f19d9716a
NikolaiLegalAI <les.contact.rassvet@gmail.com>
2025-11-26 05:24:29 -0800

    Fix env.py: correct import Base from app.database

--- backend/app/migrations/env.py
+++ backend/app/migrations/env.py
@@ -3,7 +3,7 @@
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
 from alembic import context
-from app.database import Basee
+from app.database import Base
 from app.models.user import User
 
 from app.models.device import Device
