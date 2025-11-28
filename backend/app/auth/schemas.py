263171f67811d492ed0793d4b127406f0c244c6e
NikolaiLegalAI <les.contact.rassvet@gmail.com>
2025-11-28 16:29:27 +0300

    Enhance user models with additional fields and token
    Added new fields to UserCreate and UserOut models, and created a Token model for authorization.

--- backend/app/auth/schemas.py
+++ backend/app/auth/schemas.py
@@ -1,11 +1,9 @@
 from typing import Optional
 from pydantic import BaseModel, EmailStr
 
-
 class UserBase(BaseModel):
     username: str
     email: EmailStr
-
 
 class UserCreate(UserBase):
     password: str
@@ -14,15 +12,33 @@ class UserCreate(UserBase):
     last_name: Optional[str] = None
     first_name: Optional[str] = None
     middle_name: Optional[str] = None
-    birth_year: Optional[int] = None
     phone: Optional[str] = None
-    country: Optional[str] = None
+    company: Optional[str] = None
+    position: Optional[str] = None
     city: Optional[str] = None
-    activity: Optional[str] = None
+    about: Optional[str] = None
 
-
-class UserOut(UserBase):
+class UserOut(BaseModel):
     id: int
+    username: str
+    email: EmailStr
+
+    last_name: Optional[str] = None
+    first_name: Optional[str] = None
+    middle_name: Optional[str] = None
+    phone: Optional[str] = None
+    company: Optional[str] = None
+    position: Optional[str] = None
+    city: Optional[str] = None
+    about: Optional[str] = None
+
+    is_active: bool
+    is_superuser: bool
 
     class Config:
         orm_mode = True
+
+# Новая модель для токена авторизации
+class Token(BaseModel):
+    access_token: str
+    token_type: str
